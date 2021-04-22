a0a3304cda450fcd7417b19f9da3568c061f464e
guccibase <tahiru@tryhungry.com>
2021-04-21 23:55:22 -0400

    new article

--- routes/articles.js
+++ routes/articles.js
@@ -1,14 +1,44 @@
 const express = require('express')
-const { model } = require('mongoose')
+const Article = require('../models/articles')
 const router = express.Router()
 
 
+
+
+
+router.post('/', async (req, res)=>{
+    const article = new Article({
+        title:req.body.title,
+        description:req.body.description,
+        markdown: req.body.markdown
+    })
+    try {
+        // res.error()
+        const newArticle = await article.save();
+        res.redirect(`/articles/${article.id}`)
+    } catch (error) {
+        res.render('new', {article:article} )
+
+    }
+
+
+
+})
+
 router.get('/', (req, res)=>{
-    res.send('Doge to the moon')
+    res.render('index')
 })
 
 router.get('/new', (req, res) => {
-    res.render('new')
+    res.render('new',{article:new Article})
 })
+
+
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+
+    res.render('show_article',{article:article})
+})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,41 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const app = express();
 const articlesRouter = require('./routes/articles')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const Articles = require('./models/articles')
 
-app.use('/articles', articlesRouter);
+
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({extended:false}))
 
 
 
-
-app.get('/', (req, res)=>{
-    const articles =[{
-        title:"Doge to the moon",
-        description: "Doge is the future of currencies" ,
-        date: new Date().toLocaleDateString()  
-                    },
-        {
-            title: "Elon tweets about doge",
-            description: "Doge is the future of currencies",
-                    date: new Date().toLocaleDateString()
-
-        },
-        {
-            title: "Doge will literaly land on the moon",
-            description: "Doge is the future of currencies", 
-            date: new Date().toLocaleDateString()
-
-        },
-        {
-            title: "Doge is falling",
-            description: "Doge is the future of currencies",
-            date: new Date().toLocaleDateString()
-
-        }];
+app.get('/', async (req, res)=>{
+    const articles = await Articles.find();
     res.render('index', {articles:articles})
 })
+app.use('/articles', articlesRouter);
 
 
 app.listen(3000)
